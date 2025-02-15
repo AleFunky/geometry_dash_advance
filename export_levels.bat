66f172ca6c31d628dcd2e7a139ae6381f0ab731b
KirikkSiSq <110482262+KirikkSiSq@users.noreply.github.com>
2025-02-15 09:26:42 +0300

    Description minor fix

--- export_levels.bat
+++ export_levels.bat
@@ -1,2 +1,4 @@
-CD /D "D:\Games\Modding_and_Homebrew\GitHub_Repositories\famidash_advance\"
-python3 export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything leveleasy test test2
+:: To add a level, add the name of the .json file (without the extension) to the list of levels
+:: Enter full path to your directory here
+CD /D "YOUR_DIRECTORY_PATH_HERE"
+export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything test test2
