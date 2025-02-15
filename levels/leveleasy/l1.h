530b02e051e2bff504f4a3b99f1837709457de92
KirikkSiSq <110482262+KirikkSiSq@users.noreply.github.com>
2025-02-15 09:14:28 +0300

    Reapply "Added Level Easy"
    This reverts commit 9597678208da3be2fb2c9896db185d0716e2ad9c.

---
+++ levels/leveleasy/l1.h
@@ -0,0 +1,4 @@
+// Level Data Header
+#define LEVELEASY_l1_TOTAL_BLOCKS 24165
+#define LEVELEASY_l1_RLE_DATA_SIZE 6852
+extern const unsigned short leveleasy_l1_level_data[LEVELEASY_l1_RLE_DATA_SIZE];
---
+++ levels/leveleasy/l2.h
@@ -0,0 +1,4 @@
+// Level Data Header
+#define LEVELEASY_l2_TOTAL_BLOCKS 24165
+#define LEVELEASY_l2_RLE_DATA_SIZE 36
+extern const unsigned short leveleasy_l2_level_data[LEVELEASY_l2_RLE_DATA_SIZE];
