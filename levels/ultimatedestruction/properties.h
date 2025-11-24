01605ca229a097bca3494a9baee751f2dfa81592
FijiAura <sebastiancatania@icloud.com>
2025-11-24 22:43:13 +0800

    Update properties.h

--- levels/ultimatedestruction/properties.h
+++ levels/ultimatedestruction/properties.h
@@ -1,5 +1,5 @@
 #pragma once
 
 // ultimatedestruction properties
-extern const unsigned int ultimatedestruction_properties[];
-extern const unsigned char ultimatedestruction_name[];
+extern const unsigned int ultiatedestruction_properties[];
+extern const unsigned char ultiatedestruction_name[];
