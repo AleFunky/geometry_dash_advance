e45a27bbe4299d7b473fdf4653e2d93dbe8600c9
AleFunky <alefunkyextra@gmail.com>
2026-01-17 17:40:50 +0100

    Update sgp endless part

--- levels/endless_parts/endlessdualforce/l2.h
+++ levels/endless_parts/endlessdualforce/l2.h
@@ -1,4 +1,4 @@
 // Level Data Header
 #define ENDLESSDUALFORCE_l2_TOTAL_BLOCKS 3780
-#define ENDLESSDUALFORCE_l2_RLE_DATA_SIZE 1084
+#define ENDLESSDUALFORCE_l2_RLE_DATA_SIZE 1092
 extern const unsigned short endlessdualforce_l2_level_data[ENDLESSDUALFORCE_l2_RLE_DATA_SIZE];
